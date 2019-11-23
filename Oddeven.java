8b684cf6cdfe7dac88521d7a2fe7728e60e3b05c
Shirisha <siri.vrs@gmail.com>
2019-11-23 18:01:51 +0530

    added one line

--- Oddeven.java
+++ Oddeven.java
@@ -2,6 +2,7 @@ class Oddeven{
 public static void main (String args()){
 int number=25;
 if(rem number%2==0){
+system.out.println("this is shirisha");
 system.out.println("number is even");}
 else{
 system.out.println("number is odd");}
