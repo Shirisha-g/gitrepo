a117bdc9f7ef569eef28b5f9301307eed2c415b9
Shirisha <siri.vrs@gmail.com>
2019-11-23 17:11:03 +0530

    added file

---
+++ Oddeven.java
@@ -0,0 +1,11 @@
+class Oddeven{
+public static void main (String args()){
+int number=25;
+if(rem number%2==0){
+//if remainder is 0,it is even number;
+system.out.println("number is even");}
+else{
+//if remainder is 1,it is odd number;
+system.out.println("number is odd");}
+}
+}
